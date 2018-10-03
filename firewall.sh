e0ae1bd7bd3931ed23938f4be6e28965a6699b1f
LeDSinh <dinhsinh.le@outlook.com>
2018-10-03 16:29:31 +0700

    Define policy for LOCAL, ALLOW, DENY :anger:

--- firewall.sh
+++ firewall.sh
@@ -33,3 +33,57 @@ finalize()
   return 0
   return 1
 }
+
+if [ "$1" == "dev" ]
+then
+  iptables() { echo "iptables $@"; }
+  finailize() { echo "finailize"; }
+fi
+
+# Init iptables
+initialize
+
+# Xac dinh cac policy truy cap - Determining policies
+iptables -P INPUT   DROP  # *Neu khong drop truoc thi k the thuc hien duoc - Dang tim nguyen nhan
+iptables -P OUTPUT  ACCEPT
+iptables -P FORWARD DROP
+
+# localhost
+# Tham chieu den host rieng cua no o local loopback
+iptables -A INPUT -i lo -j ACCEPT # SELF -> SELF
+
+if [ "$LOCAL_NET" ]
+# Neu bien LOCAL_NET duoc thiet lap se cho phep giao tiep voi cac server khac trong LAN
+# When $ LOCAL_NET is set, it permits communication with other servers on the LAN
+then
+  iptables -A INPUT -p tcp -s $LOCAL_NET -j ACCEPT # LOCAL_NET -> SELF
+fi
+
+# Ket thuc phan set cho localhost - localhost end
+
+# Cac host tin cay - trusted host
+if [ "${ALLOW_HOSTS}" ]
+# Neu ALLOW_HOSTS duoc thiet lap, cho phep host duoc phep truy cap
+# If $ALLOW_HOSTS is set, permission to the host is permitted
+then
+  for allow_host in ${ALLOW_HOSTS[@]}
+  do
+    iptables -A INPUT -p tcp -s $allow_host -j ACCEPT # allow_host -> SELF
+  done
+fi
+
+# Ket thuc phan set cho ALLOW_HOST - ALLOW_HOST end
+
+if [ "${DENY_HOSTS}" ]
+# just deny it :))
+then
+  for deny_host in ${DENY_HOSTS[@]}
+  do
+    iptables -A INPUT -s $deny_host -m limit --limit 1/s -j LOG --log-prefix "deny_host: "
+    iptables -A INPUT -s $deny_host -j DROP
+  done
+fi
+
+# Ket thuc phan set cho DENY_HOST, DENY HOST end
+
+
